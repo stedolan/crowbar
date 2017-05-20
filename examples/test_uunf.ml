b5db50b21f5b0a7dd1b961f6adb90cfd7ed636a8
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2017-05-20 10:28:04 +0100

    Uunf example

---
+++ examples/test_uunf.ml
@@ -0,0 +1,75 @@
+open Crowbar
+let uchar =
+  Map ([int32], fun n ->
+    let n = Int32.to_int n land 0x1FFFFF in
+    (try
+      ignore (Uchar.of_int n)
+    with
+      Invalid_argument _ -> bad_test ());
+    n)
+
+let unicode = List uchar
+
+let norm form str =
+  let n = Uunf.create form in
+  let rec add acc v = match Uunf.add n v with
+    | `Uchar u -> add (u :: acc) `Await
+    | `Await | `End -> acc in
+  let rec go acc = function
+    | [] -> List.rev (add acc `End)
+    | (v :: vs) -> go (add acc (`Uchar v)) vs in
+  go [] str
+
+let unicode_to_string s =
+  let b = Buffer.create 10 in
+  List.iter (Uutf.Buffer.add_utf_8 b) s;
+  Buffer.contents b
+
+let pp_unicode ppf s =
+  Format.fprintf ppf "@[<hov 1>[";
+  s |> List.iter (fun u ->
+           Format.fprintf ppf "U+%x@ " u
+           );
+  Format.fprintf ppf "\"%s\"" (unicode_to_string s);
+  Format.fprintf ppf "]@]"
+let unicode = Print(pp_unicode, unicode)
+
+let () =
+  add_test ~name:"uunf" [unicode] @@ fun s ->
+    let nfc = norm `NFC s in
+    let nfd = norm `NFD s in
+    let nfkc = norm `NFKC s in
+    let nfkd = norm `NFKD s in
+(*    [s; nfc; nfd; nfkc; nfkd] |> List.iter (fun s ->
+    Printf.printf "[%s]\n" (unicode_to_string s));
+    Printf.printf "\n%!";*)
+
+    let tests =
+      [
+        nfc, [
+          norm `NFC nfc;
+          norm `NFC nfd];
+        
+        nfd, [
+            norm `NFD nfc;
+            norm `NFD nfd];
+        
+        nfkc, [
+            norm `NFC nfkc;
+            norm `NFC nfkd;
+            norm `NFKC nfc;
+            norm `NFKC nfd;
+            norm `NFKC nfkc;
+            norm `NFKC nfkd];
+        
+        nfkd, [
+            norm `NFD nfkc;
+            norm `NFD nfkd;
+            norm `NFKD nfc;
+            norm `NFKD nfd;
+            norm `NFKD nfkc;
+            norm `NFKD nfkd]
+      ] in
+    (* check_eq ~pp:(pp_list pp_unicode) [nfd; nfkc] [nfd; (norm `NFKC nfd)]; *)
+    check (List.for_all (fun (s, eqs) ->
+      List.for_all (fun s' -> s = s') eqs) tests)
