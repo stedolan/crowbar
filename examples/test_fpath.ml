bfd1ffaa6fd52ef93dd8c3bee14457c76860ecdd
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2017-05-20 10:29:12 +0100

    more Fpath testing

--- examples/test_fpath.ml
+++ examples/test_fpath.ml
@@ -11,4 +11,8 @@ let fpath =
 
 let () =
   add_test ~name:"segs" [fpath] @@ fun p ->
+    let np = normalize p in
+    assert (is_dir_path p = is_dir_path np);
+    assert (is_file_path p = is_file_path np);
+    assert (filename p = filename np);
     check_eq ~eq:equal p (v @@ (fst @@ split_volume p) ^ (String.concat ~sep:dir_sep (segs p)))
